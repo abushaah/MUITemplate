3f300e5151b40b6694db5f79f1dbfa845eff80ad
abushaah <95188023+abushaah@users.noreply.github.com>
2025-06-11 16:25:57 -0400

    Delete frontend/app/routes.jsx

--- frontend/app/routes.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Routes } from 'react-router-dom';
-import { CreateRoutes } from '../src/utils/CreateRoutes';
-
-const AppRoutes = () => <Routes>{CreateRoutes()}</Routes>;
-
-export default AppRoutes;
