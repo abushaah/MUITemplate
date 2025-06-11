8dd49e9ac89432797fd9cd6886d844af3fcc53de
Haifaa Abushaaban <haifaa.abushaaban@hotmail.com>
2025-06-11 14:26:43 -0400

    renaming x1

--- frontend/app/routes.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Routes } from 'react-router-dom';
-import { createRoutes } from '../src/utils/createRoutes';
-
-const AppRoutes = () => <Routes>{createRoutes()}</Routes>;
-
-export default AppRoutes;
